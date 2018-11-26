d952b762eb7d0931d6954646aed54c1b423c374f
jaimefdez96 <jfs_x@hotmail.com>
2018-11-26 18:46:01 +0100

    Tests superados, sin modificaciones en el Descuento original

--- DescuentoBlackFridayTest.java
+++ DescuentoBlackFridayTest.java
@@ -1,50 +1,37 @@
-//
-// Instrucciones de compilación:
-// javac -cp .:./hamcrest-core-1.3.jar:./junit-4.12.jar MinTest.java
-//
-
-import static org.junit.Assert.*; // En este paquete están los métodos assertTrue, assertEquals, assertNull, fail, etc.
+import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
-public class DescuentoBlackFridayTest
-{
+public class DescuentoBlackFridayTest {
 
-	// // Test con precio entero (0 céntimos)
-	// @Test
-	// public void testForZeroDecimal ()
-	// {
-	// 	assertTrue("testForZeroDecimal_Error",		// Se imprimirá este msg si test falla
-	// 		42.0 == DescuentoBlackFriday.PrecioFinal(60.0));
-	// }
-	//
-	// //Test con precio orignal decimal (céntimos != 0)
-	// @Test
-	// public void testForDecimal()
-	// {
-	// 	assertTrue("testForDecimal_Error",		// Se imprimirá este msg si test falla
-	// 		41.93 == DescuentoBlackFriday.PrecioFinal(59.90));
-	// }
+	private double price;
+	private Date date = new Date();
+	private LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	DescuentoBlackFriday dbf = new DescuentoBlackFriday();
 
-	//Test con precio original negativo
-	@Test (expected = IllegalArgumentException.class) // Este test decora la anotación @Test con la clase de la excepción esperada.
-	public void testForNegativePrice()
-	{
-		DescuentoBlackFriday.PrecioFinal(-1.0);
-	}
-
-	//Test con precio original cero
-	@Test (expected = IllegalArgumentException.class)
-	public void testForZeroPrice()
-	{
-		DescuentoBlackFriday.PrecioFinal(0.0);
-	}
-
-	// Test con precio original válido
+	//Test 1
 	@Test
-	public void testForValidPrice()
-	{
-		assertTrue("testForValidPrice_Error", 70.0 == DescuentoBlackFriday.PrecioFinal(100.0)); // Se imprimirá este msg si test falla
+	public void testCheckPrice(){
+		price = 30.0;
+
+		int month = localDate.getMonthValue();
+		int day   = localDate.getDayOfMonth();
+		double discount = price * (30.0/100.0);
+		if((month == 11) && (day == 23)){
+				assertTrue("Es Black Friday, fallo", discount == dbf.PrecioFinal(price));
+		}else{
+				assertTrue("No es Black Friday, fallo", price == dbf.PrecioFinal(price));
+		}
+
 	}
 
+	//Test 2
+	@Test (expected = IllegalArgumentException.class)
+	public void testNonPositivePrice(){
+		price = -59.99;
+		dbf.PrecioFinal(price);
+
+	}
 }
